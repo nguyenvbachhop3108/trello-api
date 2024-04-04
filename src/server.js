b52927ab87196d00226bf03de4991c08318e7ed7
nguyenvbachhop3108 <nguyenvbachhop3108@gmail.com>
2024-04-04 21:22:39 +0700

    Express Routing

--- src/server.js
+++ src/server.js
@@ -2,16 +2,14 @@ import express from "express";
 import "dotenv/config";
 import { CONNECT_DB, GET_DB, CLOSE_DB } from "~/config/mongodb";
 import AsyncExitHook from "async-exit-hook";
+import { APIs_V1 } from "./routes/v1";
 
 const app = express();
 
 const STAR_SERVER = () => {
-  app.get("/", async (req, res) => {
-    console.log(await GET_DB().listCollections().toArray());
-    res.end("<h1>Hello World!</h1><hr>");
-  });
+  app.use("/v1", APIs_V1);
 
-  app.listen(process.env.PORT, () => {
+  app.listen(process.env.APP_PORT, () => {
     // eslint-disable-next-line no-console
     console.log(`Server is running on PORT: ${process.env.APP_PORT}`);
   });
